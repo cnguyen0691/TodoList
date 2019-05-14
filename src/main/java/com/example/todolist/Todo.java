676d7050eed9358983fb056a8efb4a2f398bde96
cnguyen0691 <chaunguyen0691@gmail.com>
2019-05-14 16:08:07 -0400

    initial

---
+++ src/main/java/com/example/todolist/Todo.java
@@ -0,0 +1,57 @@
+package com.example.todolist;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private int duedate;
+
+    @NotNull
+    private String priority;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(int duedate) {
+        this.duedate = duedate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
